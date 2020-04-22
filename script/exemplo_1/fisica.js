3a1d87e05e4f8d6d106646678a754405460a0840
GustavoGarciaPereira <gusgurtavo@gmail.com>
2020-04-22 19:29:00 -0300

    ajustando a tela

--- script/exemplo_1/fisica.js
+++ script/exemplo_1/fisica.js
@@ -1,4 +1,6 @@
 var canvas = document.getElementById('myCanvas');
+
+canvas.width = screen.width - 40
 var ctx = canvas.getContext('2d');
 
 var ball = {
@@ -18,10 +20,7 @@ var ball = {
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fillStyle = this.color;
-    ctx.fill();
-    
-    //window.requestAnimationFrame(this.draw());
-    
+    ctx.fill();    
   }
 };
 
